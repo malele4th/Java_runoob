abd694f01ccdbe4237ca250daedf7a25ec56a6bb
malele4th <malele19931203@gmail.com>
2019-04-28 19:29:13 +0800

    add data structure

---
+++ datastructure/src/map/HashMapDemo.java
@@ -0,0 +1,15 @@
+package map;
+
+import java.util.*;
+
+public class HashMapDemo {
+   public static void main(String[] args) {
+      Map m1 = new HashMap(); 
+      m1.put("Zara", "8");
+      m1.put("Mahnaz", "31");
+      m1.put("Ayan", "12");
+      m1.put("Daisy", "14");
+      System.out.println("Map Elements");
+      System.out.print(m1);
+   }
+}
